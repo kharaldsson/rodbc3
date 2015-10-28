a82fea64952416379e2c66f1336d2d5a4339520a
kharaldsson <karl.haraldsson@gmail.com>
2015-10-28 11:38:09 -0500

    changes to rodbc master

--- SQL_to_ODBC_Sanitized.R
+++ SQL_to_ODBC_Sanitized.R
@@ -1,4 +1,4 @@
 library(RODBC)
-channel <- odbcConnect("RODBCTest", uid="WINDOWS/SQL UserName", pwd="Password")
+channel <- odbcConnect("RODBCTest", uid="UserName", pwd="Password")
 charges <- sqlQuery(channel, "SELECT * FROM dbname")
 odbcClose(channel)
